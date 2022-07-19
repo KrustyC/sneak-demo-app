ff83c6ec663ce228a8143a9146f3248bfc8f8322
Davide Crestini <davide.crestini94@gmail.com>
2022-07-19 17:05:16 +0100

    feat: updated contract address

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
 import contract from "../contracts/EthereumNFT.json";
 
-export const CONTRACT_ADDRESS = "0x355638a4eCcb777794257f22f50c289d4189F245";
+export const CONTRACT_ADDRESS = "0xEA3CFc0Be47090601e9a4232CB022aD9468cC83d";
 export const CONTRACT_ABI = contract.abi;
